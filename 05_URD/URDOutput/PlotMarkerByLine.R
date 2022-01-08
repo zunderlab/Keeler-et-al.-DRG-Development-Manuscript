2d45aaad5ed76c3fa2eb96f201085f8f74ca157b
corwms <cmw6pa@virginia.edu>
2022-01-07 20:32:35 -0500

    Remove ZunderPipelineFunctions

--- 05_URD/URDOutput/PlotMarkerByLine.R
+++ 05_URD/URDOutput/PlotMarkerByLine.R
@@ -6,9 +6,160 @@
 rm(list = ls())
 .libPaths( c( .libPaths(), "~/R/x86_64-pc-linux-gnu-library/3.6") )
 
-library(ZunderPipelineFunctions)
 library(dplyr)
 library(ggfortify)
+
+#Functions for Zunder lab pipeline
+#' Metadata file input
+#'
+#' Reads metadata file and produces format friendly to scripts in Zunder lab pipeline. Metadata
+#' strategy inspired by Nowicka et al., 2017, F1000 Research
+#' @param input.folder directory containing metadata file
+#' @param md.filename metadata filename, defaults to metadata.csv as outputted by generation script
+#' @importFrom utils read.csv
+#' @export
+read.metadata <- function(input.folder,md.filename = "/metadata.csv"){
+  md <- read.csv(paste0(input.folder,md.filename))
+  #make filenames character vectors
+  md$file_name <- as.character(md$file_name)
+  return(md)
+}
+
+#' Panel file input
+#'
+#' Reads panel file and produces format friendly to scripts in Zunder lab pipeline
+#' @param input.folder directory containing metadata file
+#' @param panel.filename panel filename, defaults to panel.csv as outputted by generation script
+#' @importFrom utils read.csv
+#' @export
+read.panel <- function(input.folder,panel.filename = "/panel.csv"){
+  panel <- read.csv(paste0(input.folder,panel.filename))
+  panel$Antigen <- gsub("-", "_", panel$Antigen)
+  panel$Antigen <- gsub("\\.", "_", panel$Antigen)
+  return(panel)
+}
+
+#' Concat_Transformed file input
+#'
+#' Reads Concat_Transformed file for use in Zunder lab pipeline
+#' @param input.folder directory containing concat_transformed file
+#' @param concat.transformed.filename concat_transformed filename, defaults to
+#' Concat_Transformed.csv as outputted by generation script
+#' @importFrom utils read.csv
+#' @export
+read.concat.transformed <- function(input.folder,
+                                    concat.transformed.filename = "/Concat_Tranformed.csv"){
+  concat.transformed <- read.csv(paste0(input.folder,concat.transformed.filename))
+  colnames(concat.transformed) <- gsub("-", "_", colnames(concat.transformed))
+  colnames(concat.transformed) <- gsub(".", "_", colnames(concat.transformed), fixed = TRUE)
+  return(concat.transformed)
+}
+
+#' Layout file input
+#'
+#' Reads uMAP or other 2D layout for plotting
+#' @param input.folder directory containing layout file
+#' @param layout.filename layout filename, defaults to UMAP_layout.csv as outputted by generation
+#'  script
+#' @importFrom utils read.csv
+#' @export
+read.layout <- function(input.folder,layout.filename = "/UMAP_layout.csv"){
+  layout <- read.csv(paste0(input.folder,layout.filename))
+  return(layout)
+}
+
+#' Cluster file input
+#'
+#' Reads clusters from pipeline
+#' @param input.folder directory containing layout file
+#' @param clusters.filename clustering replicate filename, defaults to clusters.csv as outputted by
+#'  generation script
+#' @importFrom utils read.csv
+#' @importFrom data.table fread
+#' @export
+read.clusters <- function(input.folder,clusters.filename = "/clusters.csv"){
+  clusters.in <- fread(paste0(input.folder,clusters.filename))
+  if (min(clusters.in)==0){
+    #Add 1 since clustering done in Python begins indexing at zero
+    clusters <- as.vector(t(clusters.in) + 1)
+  } else{
+    clusters <- as.vector(t(clusters.in))
+  }
+  
+  return(clusters)
+}
+
+#' Clustering replicate file input
+#'
+#' Reads clustering replicates for consensus clustering and stability analysis
+#' @param input.folder directory containing layout file
+#' @param clustering.rep.filename clustering replicate filename, defaults to
+#' ClusterStabilityCheck.csv as outputted by generation script
+#' @importFrom utils read.csv
+#' @export
+read.clustering.rep <- function(input.folder,clustering.rep.filename = "/ClusterStabilityCheck.csv")
+{
+  #Add 1 since clustering done in Python begins indexing at zero
+  clustering.rep <- read.csv(paste0(input.folder,clustering.rep.filename),header = FALSE) + 1
+  return(clustering.rep)
+}
+
+#' Cluster stability file input
+#'
+#' Reads cluster stability values
+#' @param input.folder directory containing layout file
+#' @param cluster.stability.filename cluster stability filename, defaults to
+#' Final_Cluster_stability.csv as outputted by generation script
+#' @importFrom utils read.csv
+#' @export
+read.cluster.stability <- function(input.folder,
+                                   cluster.stability.filename = "/Final_Cluster_Stability.csv"){
+  cluster.stability <- as.vector(read.csv(paste0(input.folder,cluster.stability.filename),
+                                          header = FALSE)[[1]])
+  return(cluster.stability)
+}
+#' Pull out markers for transformation
+#'
+#' Finds markers in panel file marked for either clustering or plotting
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.transform.markers <- function(panel){
+  transform.markers <- as.character(panel$Metal[panel$Clustering == 1 | panel$Plotting == 1])
+  return(transform.markers)
+}
+
+#' Get marker names in more legibile format for Concat_Transformed file
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.transform.annotate <- function(panel){
+  transform.markers.annotate <- as.character(paste0(panel$Antigen[panel$Clustering == 1 |
+                                                                    panel$Plotting == 1],
+                                                    "_",panel$Metal[panel$Clustering == 1 |
+                                                                      panel$Plotting == 1]))
+  return(transform.markers.annotate)
+}
+
+#' Get clustering variables in legible format
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.clustering.annotate <- function(panel){
+  clustering.markers.annotate <- as.character(paste0(panel$Antigen[panel$Clustering == 1],"_",
+                                                     panel$Metal[panel$Clustering == 1]))
+  return(clustering.markers.annotate)
+}
+
+#' Get panel variables in legible format
+#' @param panel panel formatted following generation by getMetadata.R and input by read.panel
+#' function
+#' @export
+get.plotting.annotate <- function(panel){
+  plotting.markers.annotate <- as.character(paste0(panel$Antigen[panel$Plotting == 1],"_",
+                                                   panel$Metal[panel$Plotting == 1]))
+  return(plotting.markers.annotate)
+}
 
 ## Input parameters ===============================================================================
 INPUT.FOLDER <- getwd()
@@ -18,8 +169,8 @@ URD_IN <- "/URD.RData"
 PANEL_IN <- "panel_URD.csv"
 URD_COLNAME <- "URD" #column name in panel for plotting markers
 START_SEGMENT <-  #Root or parent segment
-END_SEGMENT <-  #tip
-PLOT_FILENAME <- "Markers_Pseudotime_.png"
+  END_SEGMENT <-  #tip
+  PLOT_FILENAME <- "Markers_Pseudotime_.png"
 
 ## Load URD workspace =============================================================================
 input.folder.temp <- INPUT.FOLDER
@@ -73,11 +224,11 @@ plotting_df_1 <- do.call(rbind,splines_markers)
 colnames(plotting_df_1) <- c("pseudotime","expression","marker")
 #output all spline curves on one plot
 ggsave("splines.png",ggplot(plotting_df_1) + geom_line(aes(x=pseudotime,y=expression,color=marker)) 
-+ scale_color_brewer(type = "qual",palette = 2) + 
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black"), 
-        axis.text = element_blank(),axis.title.y = element_text(size = 8),
-        axis.title.x = element_text(size = 8)))
+       + scale_color_brewer(type = "qual",palette = 2) + 
+         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+               panel.background = element_blank(), axis.line = element_line(colour = "black"), 
+               axis.text = element_blank(),axis.title.y = element_text(size = 8),
+               axis.title.x = element_text(size = 8)))
 
 #generate df for plotting points
 plotting_df_2 <- data.frame(pseudotime_trajectory,as.matrix(exprs_trajectory))
